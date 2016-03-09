7ffeadfacbf2e25aeb8b03954176a7da5d553a8b
Sudipta Bhowmik <sudiptab@gmail.com>
2016-03-08 18:15:16 -0800

    First commit

---
+++ InstaClone/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaClone
+//
+//  Created by Sudipta Bhowmik on 3/8/16.
+//  Copyright © 2016 Sudipta Bhowmik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
